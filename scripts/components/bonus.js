474bed89b96589f17b8a246478dfc9edff1bb1f5
zdan3v1ch <anatoli.zdanevitch@gmail.com>
2026-01-26 22:09:59 +0300

    init: init rocketP

---
+++ scripts/components/bonus.js
@@ -0,0 +1,23 @@
+import { qs, createEl } from "../utils/dom.js";
+import { navigateToOffer } from "../utils/navigation.js";
+import { appState } from "../state/app-state.js";
+
+export function renderBonus() {
+  const mount = qs("#bonus");
+  if (!mount) return;
+
+  const desktop = qs(".bonus-text.desktop span");
+  desktop.innerHTML = `${appState.offer?.bonuses?.welcome_bonus || ""}`;
+
+  const mobile = qs(".bonus-text.mobile span");
+  mobile.innerHTML = `${appState.offer?.bonuses?.welcome_bonus || ""}`;
+
+  const action = qs(".bonusButton");
+  if (appState.offer) action.setAttribute("href", `/casino/${appState.offer.id}`);
+  action.addEventListener("click", (ev) => {
+    ev.preventDefault();
+    if (appState.offer) {
+      navigateToOffer(appState.offer.id, appState.offer.link);
+    }
+  });
+}
---
+++ scripts/config/config.js
@@ -0,0 +1,2 @@
+export const SITE_ID = 28;
+export const siteName = "RocketPlay Casino AU";
