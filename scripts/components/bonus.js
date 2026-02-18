6101c7952ce5a11476bb9468ac701c1d0b11d6bb
zdan3v1ch <anatoli.zdanevitch@gmail.com>
2026-02-18 15:16:32 +0300

    init: init project

---
+++ scripts/components/bonus.js
@@ -0,0 +1,25 @@
+import { qs, createEl } from "../utils/dom.js";
+import { navigateToOffer } from "../utils/navigation.js";
+import { appState } from "../state/app-state.js";
+
+export function renderBonus() {
+  const mount = qs("#bonus");
+  if (!mount) return;
+
+  const desktop = qs(".bonus-text.desktop span");
+  if (desktop) desktop.innerHTML = `${appState.offer?.bonuses?.welcome_bonus || ""}`;
+
+  const mobile = qs(".bonus-text.mobile span");
+  if (mobile) mobile.innerHTML = `${appState.offer?.bonuses?.welcome_bonus || ""}`;
+
+  const action = qs(".bonusButton");
+  if (action) {
+    if (appState.offer) action.setAttribute("href", `/casino/${appState.offer.id}`);
+    action.addEventListener("click", (ev) => {
+    ev.preventDefault();
+    if (appState.offer) {
+      navigateToOffer(appState.offer.id, appState.offer.link);
+    }
+  });
+  }
+}
